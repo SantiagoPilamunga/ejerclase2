6dc7baca69925bf990da90c9920004b01940dbb0
Santiago <santiago.pilamunga@udla.edu.ec>
2023-11-21 14:57:09 -0500

    cambios

---
+++ ejer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n,m;
+    printf("Ingrese el valor para filas: ");
+    scanf("%d", &n);
+    printf("Ingrese el valor para columnas: ");
+    scanf("%d", &m);
+    int matrix [n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix [i][j]=0;
+        }
+    }
+    return 0;
+}
